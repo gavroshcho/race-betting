32f4bfcabdfdaaaa0c239adba8ea01a52fe78ca8
gavroshcho <nikolaivylchanov@gmail.com>
2016-09-17 12:18:40 +0300

    Calculate when they are in right order

--- lib/race.rb
+++ lib/race.rb
@@ -1,9 +1,19 @@
 module RaceBet
   class Race
-
     class << self
-      def score(guesses, winners)
+      def calculate_correct_place
+        {0 => 15, 1 => 10, 2 => 5, 3 => 3, 4 => 1}
+      end
 
+      def score(guesses, winners)
+        sum = 0
+        guesses = guesses.take(5)
+        guesses.each_with_index do |gues, index|
+          if gues == winners[index]
+            sum = sum + calculate_correct_place[index]
+          end
+        end
+        sum
       end
     end
 
